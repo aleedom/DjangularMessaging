c7851ff7698d90f619b3237dac822b144e0ebb30
Artemis <aleeodm25@gmail.com>
2016-02-17 22:06:15 -0500

    install django-rest-auth

--- Messaging/settings.py
+++ Messaging/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'rest_framework.authtoken',
+    'rest_auth',
 ]
 
 MIDDLEWARE_CLASSES = [
