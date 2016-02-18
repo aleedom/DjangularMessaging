d45e393f9e26a77b5df317eccf036dcbe1a8a8f7
Artemis <aleeodm25@gmail.com>
2016-02-17 21:50:56 -0500

    base project working

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "messaging.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Messaging.settings")
 
     from django.core.management import execute_from_command_line
 
--- messaging/urls.py
+++
@@ -1,9 +0,0 @@
-"""
-messaging URL Configuration
-"""
-from django.conf.urls import url
-from django.contrib import admin
-
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-]
